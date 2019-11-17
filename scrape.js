311d2c345069770bb20643e8a33be0b87419479d
Sara Karsh <saralkarsh@gmail.com>
2019-11-17 14:24:36 -0700

    Write scraper for Orvis stocking stuffers

--- scrape.js
+++ scrape.js
@@ -3,3 +3,28 @@
 //click on the stocking stuffers
 //select for first 10?
 //console log results - class TNAIL_PFName
+
+var Nightmare = require('nightmare')
+var nightmare = Nightmare({ show: true })
+
+nightmare
+  .goto('https://orvis.com/gifts')
+  .wait(3000)
+  .click("a[href='https://www.orvis.com/store/../stocking-stuffers']")
+  .wait(1000)
+  // .click("a[href='https://www.orvis.com/store/../stocking-stuffers']")
+  .evaluate(function() {
+    var giftNames = document.querySelectorAll('.TNAIL_PFName')
+    var stockingStuffers = [].slice.call(giftNames)
+    return stockingStuffers.map(function(item) {
+      return item.innerText
+    })
+  })
+  .end()
+  .then(function (result) {
+    console.log(result)
+  })
+    .catch(function(error) {
+      console.error('Search failed:', error)
+    })
+
